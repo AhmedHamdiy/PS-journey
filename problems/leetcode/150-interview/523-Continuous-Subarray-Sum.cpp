7cc03a22cee5ac1b7454f03b1573728922739fb2
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-06-08 04:32:56 +0300

    [523] [Time Beats: 96.04%] [Memory Beats: 31.96%] - LeetPush

--- problems/leetcode/150-interview/523-Continuous-Subarray-Sum.cpp
+++ problems/leetcode/150-interview/523-Continuous-Subarray-Sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        mp[0] = -1; 
-        int sum = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            sum += nums[i];
-            int rem = sum % k;
-            if (mp.find(rem) != mp.end()) {
-                if (i - mp[rem] > 1) {
-                    return true;
-                }
-            } else {
-                mp[rem] = i;
-            }
+     unordered_map<int,int>mp;
+     mp[0]=-1;
+     int sum=0;
+     for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+        if(mp.find(sum%k)!=mp.end()){
+            if(i-mp[sum%k]>=2)
+                return true;       
         }
-        return false;
+        else
+            mp[sum%k]=i;
+     }
+     return false;
     }
 };

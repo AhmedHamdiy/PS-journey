b1cb8b06c4b8dac678ae9f4efc2aafd779fcf0bc
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-06-13 12:35:29 +0300

    [2037] [Time Beats: 100.00%] [Memory Beats: 85.21%] - LeetPush

---
+++ problems/leetcode/150-interview/2037-Minimum-Number-of-Moves-to-Seat-Everyone.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+     sort(seats.begin(),seats.end());   
+     sort(students.begin(),students.end());
+     int ans=0,n=seats.size();
+     for(int i=0;i<n;i++){
+        ans+=abs(seats[i]-students[i]);
+     }
+     return ans;
+    }
+};

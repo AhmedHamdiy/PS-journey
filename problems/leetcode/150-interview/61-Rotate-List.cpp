8fded8359a20fcc59abc7ccac24dd17ca6cb6c5e
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-06-27 00:51:04 +0300

    [61] [Time Beats: 83.61%] [Memory Beats: 94.21%] - LeetPush

---
+++ problems/leetcode/150-interview/61-Rotate-List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)
+            return head;
+        ListNode* end=head;
+        int length=1;
+        while(end->next){
+            end=end->next;
+            length++;
+        }
+        end->next=head;
+        length-=(k%length);
+        while(length>0){
+            end=end->next;
+            head=head->next;
+            length--;
+        }
+        end->next=nullptr;
+        return head;
+    }
+};

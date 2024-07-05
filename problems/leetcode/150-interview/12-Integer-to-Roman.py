71d64004dde2d22368edb5efcc702e5646983341
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-07-05 22:36:40 +0300

    [12] [Time Beats: 97.93%] [Memory Beats: 40.94%] - LeetPush

---
+++ problems/leetcode/150-interview/12-Integer-to-Roman.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        mp = {
+            1: \I\,
+            5: \V\,    
+            4: \IV\,
+            10: \X\,   
+            9: \IX\,
+            50: \L\,   
+            40: \XL\,
+            100: \C\,  
+            90: \XC\,
+            500: \D\,  
+            400: \CD\,
+            1000: \M\, 
+            900: \CM\,
+        }
+        s = ''
+        
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                s += mp[n]
+                num-=n
+        return s

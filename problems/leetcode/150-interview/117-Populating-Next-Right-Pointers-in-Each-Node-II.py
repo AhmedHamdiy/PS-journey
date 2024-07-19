7b4fd4a1368f67dc949a6ba3c23eff44eb373522
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-07-20 01:11:23 +0300

    [117] [Time Beats: 72.50%] [Memory Beats: 7.69%] - LeetPush

---
+++ problems/leetcode/150-interview/117-Populating-Next-Right-Pointers-in-Each-Node-II.py
@@ -0,0 +1,29 @@
+\\\\\
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+\\\
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        dummy = Node(-1, None, None, None)
+        tmp = dummy
+        res = root
+        while root:
+            while root:
+                if root.left:
+                    tmp.next = root.left
+                    tmp = tmp.next
+                if root.right:
+                    tmp.next = root.right
+                    tmp = tmp.next
+                root = root.next
+            root = dummy.next
+            tmp = dummy
+            dummy.next = None
+            
+        return res

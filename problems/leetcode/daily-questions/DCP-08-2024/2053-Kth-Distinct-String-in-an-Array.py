8fdfa621ffa37e130b943f024c5c865e25f9c2c1
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-08-05 04:50:19 +0300

    [2053] [Time Beats: 89.39%] [Memory Beats: 35.47%] - LeetPush

---
+++ problems/leetcode/daily-questions/DCP-08-2024/2053-Kth-Distinct-String-in-an-Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        cnt = Counter(arr)
+        for s in arr:
+            if cnt[s]==1 :
+                k-=1
+                if k==0:
+                    return s
+        return ''

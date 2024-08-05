b37f584038f1dc5d6a8a1b6b62c4e68f0ef62c6e
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-08-05 06:23:49 +0300

    [221] [Time Beats: 85.75%] [Memory Beats: 69.27%] - LeetPush

---
+++ problems/leetcode/150-interview/221-Maximal-Square.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        ans = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                    ans = max(ans, dp[i][j])
+        return ans ** 2

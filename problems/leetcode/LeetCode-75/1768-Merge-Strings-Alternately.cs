4e539cb1a97eca7d70db25659b28a0772a69ece3
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-08-28 13:27:30 +0300

    [1768] [Time Beats: 85.24%] [Memory Beats: 22.75%] - LeetPush

---
+++ problems/leetcode/LeetCode-75/1768-Merge-Strings-Alternately.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        string s="";
+        int i=0,j=0,n=word1.Length,m=word2.Length;
+        while(i<n&&j<m){
+            s= s+word1.Substring(i,1)+word2.Substring(j,1);
+            i++;
+            j++;
+        }
+        while(i<n){
+            s= s+word1.Substring(i,1);
+            i++;
+        }
+        while(j<m){
+            s= s+word2.Substring(j,1);
+            j++;
+        }
+        return s;
+    }
+}

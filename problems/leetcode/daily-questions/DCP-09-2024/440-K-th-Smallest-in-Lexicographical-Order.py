a0503fe2dd535eb33edc6dd3d3e87edbd90b9450
Ahmed Hamdy <111378492+AhmedHamdiy@users.noreply.github.com>
2024-09-22 23:03:11 +0300

    [440] [Time Beats: 96.76%] [Memory Beats: 32.87%] - LeetPush

---
+++ problems/leetcode/daily-questions/DCP-09-2024/440-K-th-Smallest-in-Lexicographical-Order.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def getReqNum(a, b, n):
+            gap = 0
+            while a <= n:
+                gap += min(n + 1, b) - a
+                a *= 10
+                b *= 10
+            return gap
+
+        num = 1
+        i = 1
+        while i < k:
+            req = getReqNum(num, num + 1, n)
+            if i + req <= k:
+                i += req
+                num += 1
+            else:
+                i += 1
+                num *= 10
+        return num
+    
